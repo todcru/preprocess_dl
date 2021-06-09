62dc767ffb614dec2293af55b77460e7d1455352
ASWIN KUMAR M <63538759+todcru@users.noreply.github.com>
2021-06-09 10:34:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,7 @@ setuptools.setup(
 	classifiers = [
 	'Programming Language :: Python :: 3',
 	'License :: OSI Aproved :: MIT License',
-	
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
 
