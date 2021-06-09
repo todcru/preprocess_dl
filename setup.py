a544a30a5acfa2192fb169f9568867f500ff02a8
ASWIN KUMAR M <63538759+todcru@users.noreply.github.com>
2021-06-09 10:33:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,4 +9,8 @@ setuptools.setup(
 	name = 'preprocess_dl',
 	include_package_data=True,
 	version = '0.1.3',
+	author = 'Aswin Kumar',
+	author_email = 'aswinkumar.ec18@bitsathy.ac.in',
+	description = 'This is preprocessing package',
+	long_description = long_description,
 	
