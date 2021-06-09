5edcac70afa5dae84ab25c6c0357621f42bd6869
ASWIN KUMAR M <63538759+todcru@users.noreply.github.com>
2021-06-09 10:33:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+  
+import setuptools
+
+with open('README.md', 'r') as file:
+	long_description = file.read()
+
+
+setuptools.setup(
+	name = 'preprocess_dl',
+	include_package_data=True,
+	version = '0.1.3',
+	
